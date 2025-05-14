d35b9ea618a9dc205c7cdb81c630ec7937aad81f
Eduardo Henrique Dalmolin Corrêa <eduardo.correa@skyinformatica.com.br>
2025-05-14 13:35:16 -0300

    Revert "Atualização do script `atualizar-appsettings.bat` para verificar a existência do arquivo `appsettings.json` e ajuste no instalador para passar parâmetros corretamente."
    This reverts commit 34144b18929dfdaf825ea7e6c758b7c3733fe54a.

--- atualizar-appsettings.bat
+++ atualizar-appsettings.bat
@@ -1,25 +1,20 @@
 @echo off
 REM Parâmetros: caminho do appsettings, diretorio, email, senha, organizacao, usuario_windows, senha_windows
 
-set "DIRETORIO=%~1"
-set "EMAIL=%~2"
-set "SENHA=%~3"
-set "ORGANIZACAO=%~4"
-set "USUARIO_WINDOWS=%~5"
-set "SENHA_WINDOWS=%~6"
-
-REM Verifica se o arquivo appsettings.json existe
-if not exist "appsettings.json" (
-    echo Arquivo appsettings.json não encontrado.
-    exit /b 1
-)
+set "APPSETTINGS=%~1"
+set "DIRETORIO=%~2"
+set "EMAIL=%~3"
+set "SENHA=%~4"
+set "ORGANIZACAO=%~5"
+set "USUARIO_WINDOWS=%~6"
+set "SENHA_WINDOWS=%~7"
 
 powershell -NoProfile -Command ^
-    "$json = Get-Content -Raw -Encoding UTF8 'appsettings.json';" ^
+    "$json = Get-Content -Raw -Encoding UTF8 '%APPSETTINGS%';" ^
     "$json = $json -replace '\"DIRETORIO_DE_DOCUMENTOS\"', '\"%DIRETORIO%\"';" ^
     "$json = $json -replace '\"EMAIL\"', '\"%EMAIL%\"';" ^
     "$json = $json -replace '\"SENHA\"', '\"%SENHA%\"';" ^
     "$json = $json -replace '\"ORGANIZACAO\"', '\"%ORGANIZACAO%\"';" ^
     "$json = $json -replace '\"USUARIO_WINDOWS\"', '\"%USUARIO_WINDOWS%\"';" ^
     "$json = $json -replace '\"SENHA_WINDOWS\"', '\"%SENHA_WINDOWS%\"';" ^
-    "Set-Content -Path 'appsettings.json' -Value $json -Encoding UTF8"
+    "Set-Content -Path '%APPSETTINGS%' -Value $json -Encoding UTF8"
